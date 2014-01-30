f60db1b4186c72ade6fd3507ea300cdc5cb0fe7c
Harlan Haskins <harlan@bryx.com>
2014-01-29 21:59:31 -0500

    Initial Commit

---
+++ BeatWalkerTests/BeatWalkerTests.m
@@ -0,0 +1,34 @@
+//
+//  BeatWalkerTests.m
+//  BeatWalkerTests
+//
+//  Created by Harlan Haskins on 1/29/14.
+//  Copyright (c) 2014 BeatWalker. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BeatWalkerTests : XCTestCase
+
+@end
+
+@implementation BeatWalkerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
